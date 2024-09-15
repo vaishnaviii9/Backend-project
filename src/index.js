612ae55fbf65edb67f4caa6b99965fcffc732404
vaishnaviii9 <vaishnavitonpe0@gmail.com>
2024-09-15 19:36:09 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ dotenv.config({
   path: './env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+      console.log(`Server is running at Port : ${process.env.PORT}`);
+      
+    }) 
+})
+.catch((err)=> {
+  console.log("MONGODB connection failed !!!!", err);
+  
+})
 
 
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler =(requestHandler)=>{
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(re,res,next))
+        .catch((err)=> next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+// const asyncHandler =(fn )=> async(req,res,next) =>{
+//     try {
+//         await  fn(req,res,next);
+
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message: err.message
+
+//         })
+//     }
+// }
